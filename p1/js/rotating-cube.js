ffcaa019bbdd3688cb759d1ee83b787b8a40fa82
ShanuSaini <shanusaini434@gmail.com>
2019-06-14 19:30:04 -0400

    Update Viewport on window resize
    Update Viewport on window resize

--- p1/js/rotating-cube.js
+++ p1/js/rotating-cube.js
@@ -38,3 +38,9 @@ var gameLoop = function(){
 }
 
 gameLoop();
+
+window.addEventListener('resize', function(){
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
